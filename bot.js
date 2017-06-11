68ef37ff09d0d4ff9ab20a23de16ecb1cd8d58ad
chloesimon <c.simon4081@gmail.com>
2017-06-11 23:09:06 +0200

    Ajout Youtube

--- bot.js
+++ bot.js
@@ -10,37 +10,59 @@
   var speed
   var description
   var numlist
-  var datehour
   var templist
   var day
-  var month 
+  var month
   var hour
   var iso
   var translate = require('@google-cloud/translate')({
     key: 'AIzaSyAsjKDAU2Yy3Qc56OR8Ydcu99DO4rFXDlk'
+  })
 
   client.on('ready', () => {
     console.log(`Logged in as ${client.user.username}!`)
   })
-
   client.on('message', msg => {
     if (msg.channel.type !== 'dm' && (config.channel !== msg.channel.id || msg.author.id === client.user.id)) return
-  if (msg.content.includes('!translate')) {
-    var messageContent = msg.content.split('!translate')[1].split('!opt')
-    var toTranslate = messageContent[0]
-    var langue = 'en'
-    translate.translate(toTranslate, langue, function (err, result) {
-      if (err) {
-        return
-      }
-      msg.channel.sendMessage(result)
-    })
-  }
-})
+    if (msg.content.includes('!translate')) {
+      var messageContent = msg.content.split('!translate')[1].split('!opt')
+      var toTranslate = messageContent[0]
+      var langue = 'en'
+      translate.translate(toTranslate, langue, function (err, result) {
+        if (err) {
+          return
+        }
+        msg.channel.sendMessage(result)
+      })
+    }
+
+    if (msg.content.match('!youtube') !== null) {
+      message = msg.content.substring(8, msg.content.length)
+      msg.channel.sendMessage('Recherche pour' + message + ' :')
+      restClient.getPromise('https://www.googleapis.com/youtube/v3/search?q=' + message + '&maxResults=3&part=snippet&key=AIzaSyDXNwjxn5Mocc2_AhT25bl5ixvoE91NAhU')
+      .catch((error) => {
+        throw error
+      })
+      .then((res) => {
+        console.log(res.data.items)
+        for (var i = 0; i < res.data.items.length; i++) {
+          var num = res.data.items[i]
+          if (num.id.kind === 'youtube#channel') {
+            var channelId = num.id.channelId
+            var url1 = 'https://www.youtube.com/channel/' + channelId
+            msg.channel.sendMessage(url1)
+          } else if (num.id.kind === 'youtube#video') {
+            var videoId = num.id.videoId
+            var url2 = 'https://www.youtube.com/watch?v=' + videoId
+            msg.channel.sendMessage(url2)
+          }
+        }
+        console.log(res.response.statusCode)
+      })
+    }
 
     if (msg.content === 'hello') {
       msg.channel.sendMessage('I know the weather')
-
     } else if (msg.content.match('!weather') !== null) {
       city = msg.content.substring(8, msg.content.length)
       restClient.getPromise('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&units=metric&lang=fr&APPID=f2a0bbeb2be940aadb681a04cb266859')
@@ -48,7 +70,7 @@
         throw error
       })
       .then((res) => {
-        msg.channel.sendMessage('Météo à '+ city + ' :')
+        msg.channel.sendMessage('Météo à ' + city + ' :')
         temperature = res.data.main.temp
         pressure = res.data.main.pressure
         humidity = res.data.main.humidity
@@ -58,31 +80,31 @@
         msg.channel.sendMessage('La pression est de ' + pressure + ' hpa.')
         msg.channel.sendMessage('L\'humidité est de ' + humidity + '%.')
         msg.channel.sendMessage('Le vent va à ' + speed + ' km/h.')
-        msg.channel.sendMessage('Description : '+ description)
+        msg.channel.sendMessage('Description : ' + description)
         console.log(res.response.statusCode)
       })
-
     } else if (msg.content.match('!forecast') !== null) {
       city = msg.content.substring(9, msg.content.length)
       restClient.getPromise('http://api.openweathermap.org/data/2.5/forecast?q=' + city + '&units=metric&lang=fr&APPID=f2a0bbeb2be940aadb681a04cb266859')
       .catch((error) => {
         throw error
       })
-
       .then((res) => {
-        msg.channel.sendMessage('Prévision sur 5 jours à '+ city + ' :')
-        numlist=0
-        for (numlist; numlist <= 40; numlist = numlist + 8 ) { 
+        msg.channel.sendMessage('Prévision sur 5 jours à ' + city + ' :')
+        numlist = 0
+        for (numlist; numlist <= 40; numlist = numlist + 8) {
           templist = res.data.list[numlist].main.temp
-          datehour=res.data.list[numlist].dt_txt
           iso = new Date(res.data.list[numlist].dt_txt)
           day = iso.getDate()
-          month = iso.getMonth()+1
+          month = iso.getMonth() + 1
           hour = iso.getHours()
-          //msg.channel.sendMessage('DATE ET HEURE :' + datehour)//
-          msg.channel.sendMessage('La temperature est de ' + templist + ' degrés '+'le ' + day +'/' + month + ' à ' + hour + 'h')
+          msg.channel.sendMessage('La temperature est de ' + templist + ' degrés ' + 'le ' + day + '/' + month + ' à ' + hour + 'h')
         }
       })
     }
   })
   client.login(config.token)
+
+
+
+
