190ed7552ea3af233c7089089c3221418bbd00b1
chloesimon <c.simon4081@gmail.com>
2017-06-12 21:12:45 +0200

    V3 master

--- bot.js
+++ bot.js
@@ -112,11 +112,85 @@
           client.user.setUsername(newnamebot)
         .then(user => console.log(`My new username is ${user.username}`))
         .catch(console.error)
-          msg.channel.sendMessage('Bonjour, je me nomme ' + JSON.stringify(result.forms[0].name, null, 2) + '! Je suis un pokemon de type ' + JSON.stringify(result.types[0].type.name, null, 2) + '. Mon id est : ' + JSON.stringify(result.id, null, 2) + '. Je pèse ' + JSON.stringify(result.weight, null, 2) + ' pounds!' + ' et je mesure : ' + JSON.stringify(result.height, null, 2) + ' pieds.')
+          msg.channel.sendMessage('Bonjour, je me nomme ' + JSON.stringify(result.forms[0].name, null, 2) + '! Je suis un pokemon de type ' + JSON.stringify(result.types[0].type.name, null, 2) + '. Mon id est : ' + JSON.stringify(result.id, null, 2) + '. Je pèse ' + JSON.stringify(result.weight, null, 2) + ' pounds' + ' et je mesure ' + JSON.stringify(result.height, null, 2) + ' pieds.')
         } else {
           console.log(error)
         }
       })
+    // SPOTIFY
+    } else if (msg.content.startsWith('!spotify ')) { // 1
+      var SpotifyWebApi = require('spotify-web-api-node')
+    // Create the api object with the credentials
+      var spotifyApi = new SpotifyWebApi({
+        clientId: '7d618a54d6e44ae1934e77160031d192',
+        clientSecret: '577a78ca518c447c879bddda7b47d883'
+      })
+      var msgRecherche = msg.content.split(' ').slice(1)
+      console.log(msgRecherche)
+    // Retrieve an access token.
+      spotifyApi.clientCredentialsGrant()
+    .then(function (data) { // 2
+      // Save the access token so that it's used in future calls
+      spotifyApi.setAccessToken(data.body['BQC-VqUiiy4FRWQqNWr8ElMkNeioY9Jq1AamYDn7NyO208Taf7ojTtbGVB-6dMd3Y-SHM0OSmuIxWshL27mO90xL4XrduZjeN76kej5QyiY_1bEK8aJt68COFVlWiU7DZXYDDnE1yRxEbmA'])
+      if (msgRecherche[0] !== '!artist' && msgRecherche[0] !== '!album' && msgRecherche[0] !== '!track') {
+        spotifyApi.search(msgRecherche.join(' '), ['artist', 'track', 'album'], {limit: 1}, function (err, data) {
+          if (err) {
+            console.log('Error occurred: ' + err)
+          } else {
+            console.log(data)
+            msg.channel.send('The track : ' + data.body.tracks.items[0].name)
+            msg.channel.send(data.body.tracks.items[0].external_urls.spotify)
+
+            msg.channel.send('The artist : ' + data.body.artists.items[0].name)
+            msg.channel.send(data.body.artists.items[0].external_urls.spotify)
+
+            msg.channel.send('The album : ' + data.body.albums.items[0].name)
+            msg.channel.send(data.body.albums.items[0].external_urls.spotify)
+          }
+        }, function (err) {
+          console.log('Something went wrong when retrieving an access token', err)
+        })
+      }
+      if (msgRecherche[0] === '!track') {
+        spotifyApi.search(msgRecherche.join(' ').substring(6), ['track'], {limit: 1}, function (err, data) {
+          if (err) {
+            console.log('Error occurred: ' + err)
+          } else {
+            console.log(data)
+            msg.channel.send('The track : ' + data.body.tracks.items[0].name)
+            msg.channel.send(data.body.tracks.items[0].external_urls.spotify)
+          }
+        }, function (err) {
+          console.log('Something went wrong when retrieving an access token', err)
+        })
+      }
+      if (msgRecherche[0] === '!artist') {
+        spotifyApi.search(msgRecherche.join(' ').substring(7), ['artist'], {limit: 1}, function (err, data) {
+          if (err) {
+            console.log('Error occurred: ' + err)
+          } else {
+            console.log(data)
+            msg.channel.send('The artist : ' + data.body.artists.items[0].name)
+            msg.channel.send(data.body.artists.items[0].external_urls.spotify)
+          }
+        }, function (err) {
+          console.log('Something went wrong when retrieving an access token', err)
+        })
+      }
+      if (msgRecherche[0] === '!album') {
+        spotifyApi.search(msgRecherche.join(' ').substring(6), ['album'], {limit: 1}, function (err, data) {
+          if (err) {
+            console.log('Error occurred: ' + err)
+          } else {
+            console.log(data)
+            msg.channel.send('The album : ' + data.body.albums.items[0].name)
+            msg.channel.send(data.body.albums.items[0].external_urls.spotify)
+          }
+        }, function (err) {
+          console.log('Something went wrong when retrieving an access token', err)
+        })
+      }
+    })
     }
     // TWITTER
     var msgtweet = msg.content
